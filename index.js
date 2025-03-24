a3a78a1b6d5e75834a586532621d253b19993456
= <=>
2025-03-24 22:31:26 +0530

    add files

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+// console.log(Boolean('false'))  //true
+// console.log(Boolean(false)) //false
+// console.log('3' + 1) //'31'
+// console.log('3' - 1) //2
+// console.log('3' - ' 02 ') //1 
+// console.log('3' * ' 02 ') //'6'
+// console.log(Number('1')) //1
+// console.log(Number('number')) //nan
+// console.log(Number(null)) //0
+// console.log(Number(false)) //0
 
-let obj = {
-    name: 'John',
-    age: 30,
-    old: false
-}
 
+
+// console.log(0 == false) //true
+// console.log('' == false) //true
+// console.log([] == false)  //true
+// console.log(undefined == false) //false
+// console.log(null == false) //false
+// console.log('1' == true) //true
+// console.log(1n == true) //true
+// console.log(' 1     ' == true) //true
